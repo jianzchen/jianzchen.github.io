8e5d069b837094ac2666f0e3064abb43c3d2edf8
jianzchen <jianzchen@ebay.com>
2015-04-10 17:40:56 +0800

    1

--- test/test.php
+++ test/test.php
@@ -1,12 +1,2 @@
-<h1>test</h1>
-<script language="php">
-    echo "test";
-</script>
 <?php
-/**
- * Created by PhpStorm.
- * User: jianzchen
- * Date: 2015/3/30
- * Time: 18:14
- */
 echo "<h1>123</h1>";
